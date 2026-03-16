a936e1a757ec76616268b3e0c0f7152044751a9e
Balusani Venkatesh <106467934+VenkyRock143@users.noreply.github.com>
2026-03-16 18:08:57 +0530

    Update validate.js

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -1,11 +1,11 @@
-const { ZodError } = require("zod");
+const { z } = require("zod");
 
 module.exports = (schema) => (req, res, next) => {
   try {
     schema.parse(req.body);
     next();
   } catch (err) {
-    if (err instanceof ZodError) {
+    if (err instanceof z.ZodError) {
       return res.status(400).json({ errors: err.errors });
     }
     next(err);
